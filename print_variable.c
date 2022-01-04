a004dd2ae9bb1cdb715277ba48ea24cb1374e73a
Hsuan9922 <90672981+Hsuan9922@users.noreply.github.com>
2022-01-04 13:03:14 +0800

    Update print_variable.c

--- print_variable.c
+++ print_variable.c
@@ -1,10 +1,14 @@
+
 #include <stdio.h>
 
 int main() {
 
     int b = 87;
     for (int j = 5; j < 23; j++) {
-            printf("j=%d, j=%d\n", j, b*j);
+            printf("j=%d, b=%d\n", j, b*j);
+        //give me some output messages in following format
+        //and that can be done by add only ONE line code.
+        //please refer to Google slides, page 22.
         //j=5, b=435
         //j=6, b=522
         //j=7, b=609
